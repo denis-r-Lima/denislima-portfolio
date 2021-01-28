d4754273f29b54a5274947f093ca4d290212e151
Denis Lima <denis.r.lima88@gmail.com>
2021-01-28 21:46:30 +0300

    Using both functions on the converter

--- pages/converter/index.tsx
+++ pages/converter/index.tsx
@@ -1,15 +1,18 @@
 import React from "react"
 
 
-import { convertFromRomans } from "../../controlers/functions"
+import { convertFromRomans , convertToRomans} from "../../controlers/functions"
 // import { Container } from './styles';
 
 const RomanNumbers: React.FC = () => {
   const [numRom, setNumRom] = React.useState<string>("")
+  const [numDec, setNumDec] = React.useState<string>("")
 
   return (
     <div>
       <h1>Numeros Romanos</h1>
+      <br/>
+      <label>Roman numerals to Decimal</label>
       <input
         type='text'
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,6 +20,14 @@ const RomanNumbers: React.FC = () => {
         }}
       />
       {convertFromRomans(numRom)}
+      <label>Decimal to Roman numerals</label>
+      <input
+        type='text'
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setNumDec(e.currentTarget.value)
+        }}
+      />
+      {convertToRomans(parseInt(numDec))}
     </div>
   )
 }
