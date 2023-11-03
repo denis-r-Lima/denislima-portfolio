bd5e0f02d3c6df95583a4bdec8a9ae5d5fcad0ff
Denis Lima <69401221+denis-r-Lima@users.noreply.github.com>
2023-11-03 17:25:49 -0500

    Update habitList.tsx

--- components/habit/HabitList/habitList.tsx
+++ components/habit/HabitList/habitList.tsx
@@ -4,13 +4,19 @@ import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 import { Card, CardTextBox, Container } from "./styles";
 import { useHabitContext } from "../../../context/HabitContext";
 
-type TodayDataType = { date: string; habits: string[]; completed: string[] };
+type TodayDataType = {
+  date: string;
+  habits: string[];
+  completed: string[];
+  notCompleted: string[];
+};
 
 const HabitList: React.FC = () => {
   const [habitData, setHabitData] = useState<TodayDataType>({
     date: "",
     habits: [],
     completed: [],
+    notCompleted: [],
   });
   const { getTodayData, updateToday, fetched, resetToday, fetchFromStore } =
     useHabitContext();
@@ -52,6 +58,9 @@ const HabitList: React.FC = () => {
       completed: completed
         ? [...habitData.completed, habitData.habits[index]]
         : [...habitData.completed],
+      notCompleted: completed
+        ? [...habitData.notCompleted]
+        : [...habitData.notCompleted, habitData.habits[index]],
     };
     const habitClosed = habitData.habits[index];
     setTimeout(() => {
@@ -79,12 +88,26 @@ const HabitList: React.FC = () => {
         </Card>
       ))}
       <br />
-      <h1>Completed Tasks:</h1>
-      {habitData.completed.map((habit) => (
-        <Card key={habit}>
-          <CardTextBox>{habit}</CardTextBox>
-        </Card>
-      ))}
+      {habitData.completed?.length > 0 && (
+        <>
+          <h1>Completed Tasks ({habitData.completed?.length})</h1>
+          {habitData.completed.map((habit) => (
+            <Card key={habit} className="completed fixed">
+              <CardTextBox>{habit}</CardTextBox>
+            </Card>
+          ))}
+        </>
+      )}
+      {habitData.notCompleted?.length > 0 && (
+        <>
+          <h1>Not Completed Tasks ({habitData.notCompleted?.length})</h1>
+          {habitData.notCompleted.map((habit) => (
+            <Card key={habit} className="incomplete fixed">
+              <CardTextBox>{habit}</CardTextBox>
+            </Card>
+          ))}
+        </>
+      )}
     </Container>
   );
 };
